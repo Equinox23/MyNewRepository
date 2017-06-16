da5ad407c12a716b84909b694e6d1ac01a88e39b
Bornatch <Bornatch@Bornatch-Lenovo>
2017-06-16 04:26:20 +0200

    pas deJunit...

---
+++ SmartPhonePOO2017/src/JUnitTest/PenelGalerie.java
@@ -0,0 +1,14 @@
+package JUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PenelGalerie {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
